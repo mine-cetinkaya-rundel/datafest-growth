2824e6b0631681887068126e0acb068dfb387707
mine-cetinkaya-rundel <mine@stat.duke.edu>
2017-05-22 23:59:51 -0400

    add radius size for each year depending on number of participants

--- code/01-data-prep.R
+++ code/01-data-prep.R
@@ -20,10 +20,42 @@ cols <- c("df_", "num_part_", "other_inst_")
 names(datafest) <- c("timestamp", "host", "city", "state", "country", "url",
                      paste0(cols, yrs))
 
-# geocode -----------------------------------------------------------
+# geocode host location ---------------------------------------------
 datafest <- datafest %>%
   mutate(address = paste(city, state, country)) %>%
   mutate_geocode(address)
+
+# calculate radius size for points on map ---------------------------
+min_part <- min(datafest$num_part_2011,
+                datafest$num_part_2012,
+                datafest$num_part_2013,
+                datafest$num_part_2014,
+                datafest$num_part_2015,
+                datafest$num_part_2017, 
+                na.rm = TRUE)
+
+max_part <- max(datafest$num_part_2011,
+                datafest$num_part_2012,
+                datafest$num_part_2013,
+                datafest$num_part_2014,
+                datafest$num_part_2015,
+                datafest$num_part_2017,
+                na.rm = TRUE)
+
+range_part <- max_part - min_part                                                                          
+
+range_step <- range_part / 10
+
+datafest <- datafest %>%
+  mutate(
+    radius_2017 = num_part_2017 / range_step,
+    radius_2016 = num_part_2016 / range_step,
+    radius_2015 = num_part_2015 / range_step,
+    radius_2014 = num_part_2014 / range_step,
+    radius_2013 = num_part_2013 / range_step,
+    radius_2012 = num_part_2012 / range_step,
+    radius_2011 = num_part_2011 / range_step
+  )
 
 # write prepped data ------------------------------------------------
 write_csv(datafest, path = "data/datafest.csv")
