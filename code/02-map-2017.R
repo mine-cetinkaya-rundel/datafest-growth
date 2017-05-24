5b2f3cfa479d1a08c152d8482a52719424e8462c
mine-cetinkaya-rundel <mine@stat.duke.edu>
2017-05-23 22:41:44 -0400

    factor popup text code and use point radii in log scale

--- code/02-map-2017.R
+++ code/02-map-2017.R
@@ -21,19 +21,30 @@ top <- ceiling(max(datafest$lat))
 
 
 # define popups -----------------------------------------------------
+host_text <- paste0(
+  "<b><a href='", datafest_2017$url, "' style='color:", href_color, "'>", datafest_2017$host, "</a></b>"
+)
+
+other_inst_text <- paste0(
+  ifelse(is.na(datafest_2017$other_inst), 
+         "", 
+         paste0("<br>", "with participation from ", datafest_2017$other_inst))
+)
+
+part_text <- paste0(
+  "<font color=", part_color,">", datafest_2017$num_part, " participants</font>"
+)
+
 popups <- paste0(
-  "<b><a href='", datafest_2017$url, "' style='color:", href_color, "'>", datafest_2017$host, "</a></b>",
-  ifelse(is.na(datafest_2017 $other_inst), "",
-         paste0("<br>", "with participation from ", datafest_2017$other_inst)),
-  "<br>", 
-  paste0("<font color=", part_color,">", datafest_2017$num_part, " participants</font>"))
+  host_text, other_inst_text, "<br>" , part_text
+)
 
 # plot map ----------------------------------------------------------
 leaflet() %>%
   addTiles() %>%
   fitBounds(lng1 = left, lat1 = bottom, lng2 = right, lat2 = top) %>%
   addCircleMarkers(lng = datafest_2017$lon, lat = datafest_2017$lat,
-                   radius = datafest_2017$radius * 1.5, 
+                   radius = log(datafest_2017$num_part) * 1.2, 
                    fillColor = marker_color,
                    color = marker_color,
                    weight = 1,
